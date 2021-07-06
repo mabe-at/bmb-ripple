a20c151437b281dd5adb52205ff0504f0e7bb58b
Dmitry Moiseev <mdmitry94@gmail.com>
2021-07-06 14:10:30 +0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Ripple demo</h1>
+  `
 })
 export class AppComponent {
-  title = 'ripple';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RippleDirective } from './ripple/ripple.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RippleDirective,
   ],
   imports: [
     BrowserModule
